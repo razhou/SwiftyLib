bcca4a3cf6f815c94c382eb01d07c8af0a703b1a
Raju Riyanda <r4ju.riy4nd4@gmail.com>
2022-12-04 21:50:58 +0700

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Macbook-M1-JDS on 02/12/22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
